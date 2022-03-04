97eca32b81feba166432abfd8a553402f6ca1ea9
Nas3nmann <nas3nmann@googlemail.com>
2022-03-04 19:45:53 +0100

    Upgrades to Angular 13 - Also updated NX and other dependencies - Changes Angular peer dependencies of library to range >=8 < 14

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
+    ...getJestProjects(),
     '<rootDir>/apps/demo-book-list',
     '<rootDir>/libs/ngx-deep-linking',
     '<rootDir>/apps/demo-form',
